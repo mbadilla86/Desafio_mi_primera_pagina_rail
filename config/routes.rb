1d347434ffbb44e110177adb1b165e67819e8bc6
Mauricio Badilla <mbadilla86@gmail.com>
2021-02-25 15:10:10 -0600

    creation of page three, title and paragraph

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'pages', to: 'pages#one'
   get 'two', to: 'pages#two'
+  get 'three', to: 'pages#three'
   root 'pages#one'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
